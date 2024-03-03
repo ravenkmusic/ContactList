723cec1a1f6fa465d40e1f8d91d251ea17c57e64
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 15:08:18 -0500

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import ContactList from './components/ContactList'
-import SelectedContact from './components/SelectedContact'
 
 function App() {
-  const [selectedContactId, setselectedContactId] = useState(null);
+  const [selectedContactId, setSelectedContactId] = useState(null);
   
   return (
     <>
       {selectedContactId ? (
         <div>Short message</div>
         ) : (
-        <ContactList setselectedContactId={setselectedContactId}/>
+        <ContactList setselectedContactId={setSelectedContactId} />
         )}
     </>
   );
