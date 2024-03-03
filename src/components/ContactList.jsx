f8767e61e6f278dbaaf5114581222e4966bc8976
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 13:24:20 -0500

    wrote fetchContacts() function to fetch actual api data

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from 'react';
 import ContactRow from "./ContactRow";
+import { useEffect } from "react";
 
     
 const dummyContacts = [
@@ -13,6 +14,20 @@ function ContactList (){
     const [contacts, setContacts] = useState(dummyContacts);
 
     console.log("Contacts: ", contacts);
+
+    // i am very pleased to say that i actually was able to come up with the useEffect logic myself, lol.
+    useEffect(()=>{
+        async function fetchContacts() {
+            try {
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json();
+                console.log(result);
+            } catch (error) {
+                console.error("Can't retrieve data", error);
+            }
+        }
+        fetchContacts();
+    },[]);
 
     return (
         <table>
