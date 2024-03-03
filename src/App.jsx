8fec98effe131709ad0a4b9ffa63fee68ae37325
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 14:40:53 -0500

    fixed typos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import ContactList from './components/ContactList'
+import SelectedContact from './components/SelectedContact'
 
 function App() {
   const [selectedContactId, setselectedContactId] = useState(null);
   
-
   return (
     <>
       {selectedContactId ? (
