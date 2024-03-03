8e78be47101d720e06b6b4d6bd12c092093348ba
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 14:45:21 -0500

    fixed file locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import ContactList from './components/ContactList'
-import SelectedContact from './components/SelectedContact'
+import SelectedContact from 'src/SelectedContact.jsx'
 
 function App() {
   const [selectedContactId, setselectedContactId] = useState(null);
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,6 +1,6 @@
 function ContactRow({ setSelectedContactId, contact }){
     return (
-        <tr  onClick={() => setSelectedContactId(contact.id)}>
+        <tr onClick={() => setSelectedContactId(contact.id)}>
             <td>{contact.name}</td>
             <td>{contact.email}</td>
             <td>{contact.phone}</td>
