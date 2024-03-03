ebb261bd4c8270e9f4574169aa8426980c883c14
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 14:23:39 -0500

    passed setselectedcontactid function as parameter

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -12,7 +12,7 @@ const dummyContacts = [
 
 const API_URL = `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`;
 
-function ContactList (){
+function ContactList (setSelectedContactId){
     const [contacts, setContacts] = useState(dummyContacts);
 
     console.log("Contacts: ", contacts);
