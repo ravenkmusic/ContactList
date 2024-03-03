5bc54f8cbeb987487f16bd8b0ff0b40a1091eb31
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-03 15:07:47 -0500

    fixed syntax error

--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,6 +1,9 @@
 function ContactRow({ setSelectedContactId, contact }){
     return (
-        <tr onClick={() => setSelectedContactId(contact.id)}>
+        <tr onClick={() => {
+            setSelectedContactId(contact.id);
+            }}
+        >
             <td>{contact.name}</td>
             <td>{contact.email}</td>
             <td>{contact.phone}</td>
